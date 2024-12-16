cdf6a3325dfcc28d0c2f4d64cc14389c666466ae
Mohit Chauhan <chauhanmohit@gmail.com>
2024-12-16 11:38:05 +0530

    new changes

--- src/components/content-generator/index.ts
+++ src/components/content-generator/index.ts
@@ -1,5 +1,4 @@
 export { ContentEditor } from './ContentEditor';
 export { PreviewScreen } from './PreviewScreen';
-export { AIChat } from './AIChat';
 export { PlatformPreview } from './PlatformPreview';
 export type { FormData } from './types';
