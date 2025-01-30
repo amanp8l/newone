3cdc09f64d4d4c8f02a5463b4b66c63e316f83ed
Mohit Chauhan <chauhanmohit@gmail.com>
2025-01-30 14:29:05 +0530

    new changes

--- src/components/content-generator/index.ts
+++ src/components/content-generator/index.ts
@@ -1,4 +1,2 @@
 export { ContentEditor } from './ContentEditor';
-export { PreviewScreen } from './PreviewScreen';
-export { PlatformPreview } from './PlatformPreview';
 export type { FormData } from './types';
